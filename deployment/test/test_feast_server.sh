f9ae3a2113ac057ddd40e1e3415bb0a75cf86de2
onishima <masayuki.onishi@cba.com.au>
2025-09-17 11:19:10 +1000

    Updates

--- deployment/test/test_feast_server.sh
+++ deployment/test/test_feast_server.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-curl -X POST "http://localhost:8999/get-online-features" -d '{
+feast serve
+curl -X POST "http://localhost:6566/get-online-features" -d '{
     "features": [
       "customer_credit_risk_feature_view:purpose_business"
     ],
